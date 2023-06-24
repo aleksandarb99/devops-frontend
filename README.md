2e95a0449cff97210edac7016114c417ab389dda
Aleksandar Buljevic <abuljevic8@gmail.com>
2023-06-24 19:00:33 +0200

    Added readme and fixed nginx.fonf

---
+++ README.md
@@ -0,0 +1,11 @@
+# devops-frontend
+
+## Requirements
+
+- Please make sure Docker Desktop is running.
+
+## Build this image locally
+
+```shell
+docker compose up --build
+```
--- accommodation-frontend/src/app/environment/environment.ts
+++ accommodation-frontend/src/app/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    USERS_URL: "http://localhost:9000",
+    USERS_URL: "http://localhost:4000",
     CLIENT_ID: "oidc-client",
     REDIRECT_URL: "http://localhost:4200/callback",
     STATE_LENGTH: 64
